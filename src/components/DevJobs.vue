73819fe8a7235bfc556f3e7340577e782be1d518
Ahmet Ayyildiz <ahmet.ayyildiz@qlik.com>
2022-07-05 15:16:24 +0200

    revert the commit

---
+++ src/components/DevJobs.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="jobs">
+    <DevJob v-for="job in jobs" :key="job.id" :job="job" />
+  </section>
+</template>
+
+<script lang="ts" setup>
+import DevJob from "./DevJob.vue";
+import jobs from "../../data.json";
+</script>
+
+<style lang="scss" scoped>
+.jobs {
+  margin-top: 1.4rem;
+}
+</style>
